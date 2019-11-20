5cf0afcf2ff978783ae0bfe1320ca96c91340274
Deniskakaka <39855163+Deniskakaka@users.noreply.github.com>
2019-11-20 11:18:34 +0200

    Create README.m

---
+++ README.m
@@ -0,0 +1,13 @@
+# claculator
+it just calculator he can * / + -;
+
+# used thechnologies
+  <ol>
+    <li>Jquery</li>
+    <li>Less</li>
+    <li>Css</li>
+    <li>HTML</li>
+  </ol>
+
+# link
+https://deniskakaka.github.io/claculator/.
